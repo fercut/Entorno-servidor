645fbc717b20c3f023ced35de5a091fca4c7b70a
FerminFernandezCutilla <119857762+FerminFernandezCutilla@users.noreply.github.com>
2023-10-03 19:00:06 +0200

    apartados

---
+++ 04-nodejs-quirk/dance-cash.js
@@ -0,0 +1,22 @@
+const MOVES = ["Shimmy", "Shake", "Pirouette", "Slide", "Box Step", "Headspin", "Dosado", "Pop", "Lock", "Arabesque"];
+
+function danceConvert(pin) {
+
+    if(pin.length<4) return 'Invalid input.';
+    if(pin===NaN) return 'Invalid input.';
+  
+	const pinNum =pin.split('').map(Number);
+    let movimiento = new Array(4);
+  
+  	for(let i= 0; i<pinNum.length; i++){
+        pinNum[i] = (pinNum[i] + i)%10;
+    }
+  console.log(pinNum)
+  
+    for(let i= 0; i<movimiento.length; i++){
+        movimiento[i] = MOVES[pinNum[i]];
+			 if(movimiento[i]=== undefined) return 'Invalid input.';
+    }
+    return movimiento;
+}
+
