6d213314542830e5d6aca9baad3d1a0f2382d0cb
FerminFernandezCutilla <119857762+FerminFernandezCutilla@users.noreply.github.com>
2023-10-30 19:47:08 +0100

    12 workspace

---
+++ 12-setup-workspace/src/index.js
@@ -0,0 +1,7 @@
+function mySuperFunction() {
+  const mySuperConst = '3';
+
+  // eslint-disable-next-line no-console
+  console.log(`This is my super var ${mySuperConst}`);
+}
+mySuperFunction(1, 1, 2, 3);
