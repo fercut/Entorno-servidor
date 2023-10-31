6f761120ad63b892bb4d22f57b0f44371f0bb22c
FerminFernandezCutilla <119857762+FerminFernandezCutilla@users.noreply.github.com>
2023-10-31 19:50:44 +0100

    13 avance

---
+++ 13-nodejs-server/src/ejercicio2.js
@@ -0,0 +1,21 @@
+import http from 'http';
+import fs from 'fs';
+
+const port = 3000;
+
+const server = http.createServer((req, resp) => {
+  fs.readFile('./public/index.html', (err, data) => {
+    if (err) {
+        resp.writeHead(404, {'Content-Type': 'text/plain'});
+        resp.end('Error 404: Not Found');
+    } else {
+        resp.writeHead(200, {'Content-Type': 'text/html'});
+        resp.end(data);
+    }
+});
+});
+
+// * Configure server
+server.listen(port, ()=>{
+    console.log(`Server is ready at http://localhost:${port}`);
+});
